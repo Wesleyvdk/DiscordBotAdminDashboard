0e257060e82e6fd6d87192dd44d2f31dc41a6c61
Wesleyvdk <76257783+Wesleyvdk@users.noreply.github.com>
2023-12-24 22:44:10 +0100

    update app/dashboard/dashboardcard.tsx

--- app/dashboard/dashboardcard.tsx
+++ app/dashboard/dashboardcard.tsx
@@ -89,7 +89,7 @@ export default async function DashboardCard({
                   onChange={(newValue) =>
                     handleSwitchChange(command.name, newValue)
                   }
-                  className="relative inline-flex h-6 w-11 items-center rounded-full ui-checked:bg-blue-600 ui-not-checked:bg-gray-200"
+                  className="relative inline-flex h-6 w-11 items-center rounded-full ui-checked:bg-blue-600 ui-not-checked:bg-gray-200 right-1.5"
                 >
                   <span className="sr-only">Enable command</span>
                   <span className="inline-block h-4 w-4 transform rounded-full bg-white transition ui-checked:translate-x-6 ui-not-checked:translate-x-1" />
